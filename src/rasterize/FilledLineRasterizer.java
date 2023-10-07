5a19187f2f596ac9260653d6b5f77b4a59bb955e
jholaj <thezmrdus@gmail.com>
2023-10-08 00:07:20 +0200

    Added algorhitm documentation

--- src/rasterize/FilledLineRasterizer.java
+++ src/rasterize/FilledLineRasterizer.java
@@ -12,7 +12,13 @@ public class FilledLineRasterizer extends LineRasterizer {
         calcMidpoint(x1, y1, x2, y2);
     }
 
-    // MIDPOINT
+    /**
+     *
+     * Algoritmus: Midpoint (Využívá půlení úseček)
+     * Výhody: Jednoduchá implementace, funkční pro všechny kvadranty
+     * Nevýhody: Rekurze (lze vyřešit zásobníkem)
+     *
+     * */
     private void calcMidpoint(int x1, int y1, int x2, int y2) {
         int sx, sy;
         sx = (x1 + x2) / 2;
